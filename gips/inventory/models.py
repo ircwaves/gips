f73e78f8ad56c89263ce5a84f401348d8322624a
pavel <pavel@pavel-gips.lxd>
2016-08-08 21:57:13 +0000

    Chande keys to allow null

--- gips/inventory/models.py
+++ gips/inventory/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     shapefile = models.CharField(max_length=1000, primary_key=True)
     created = models.DateTimeField(editable=False, auto_now_add=True)
     modified = models.DateTimeField(editable=False, auto_now=True)
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, null=True)
 
     def __unicode__(self):
         return self.shapefile
@@ -15,7 +15,7 @@ class FileAsset(models.Model):
     filepath = models.CharField(max_length=1000, primary_key=True)
     product = models.CharField(max_length=256, null=True, blank=True)
     sensor = models.CharField(max_length=256, null=True, blank=True)
-    project = models.ForeignKey(Project)
+    project = models.ForeignKey(Project, null=True)
     created = models.DateTimeField(editable=False, auto_now_add=True)
     modified = models.DateTimeField(editable=False, auto_now=True)
 
@@ -24,6 +24,7 @@ class FileAsset(models.Model):
 
 
 class Command(models.Model):
+    user = models.ForeignKey(User, null=True)
     command = models.TextField(primary_key=True)
     created = models.DateTimeField(editable=False, auto_now_add=True)
 
