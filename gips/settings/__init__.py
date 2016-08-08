688be593819b41d0ac7cc3e4d22345ffdb044d42
pavel <pavel@pavel-gips.lxd>
2016-08-08 20:20:00 +0000

    Using the base settings and default settings file now

--- gips/settings/__init__.py
+++ gips/settings/__init__.py
@@ -1,7 +1,28 @@
-import sys
+import os, sys, glob, importlib
+
+DEFAULT = 'default'
 
 try:
     from .base import *
 except ImportError:
-    print 'You have no settings file configured. Copy template.py to base.py and modify for your use.'
-    sys.exit()    
+    print 'You have no base settings file, but it is required.'
+    sys.exit(1)
+
+
+settings_module = os.environ.get('GIPS_SETTINGS_FILE', DEFAULT)
+
+_cwd = os.path.realpath(os.path.dirname(os.path.abspath(__file__)))
+
+files = glob.glob(_cwd+"/*.py")
+modules = map(lambda s: os.path.basename(s)[:-3], files)
+
+if settings_module in modules:
+    try:
+        eval('from {} import *'.format(settings_module))
+    except ImportError, OSError:
+        print 'Error importing settings from {}.py'.format(settings_module)
+        sys.exit(1)
+
+else:
+    print 'Settings file {}/{}.py not found.'.format(_cwd, settings_module)
+    sys.exit(1)
